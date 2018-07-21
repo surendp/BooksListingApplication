b9f661b2ddbc47e212b23d076651e2d1d96f93ec
Surendra Pandey <surendra.pandey2@metropolia.fi>
2018-07-21 11:56:06 +0300

    Display lists of books listed on books reducer.

---
+++ src/containers/book-list.js
@@ -0,0 +1,39 @@
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import {selectBook} from '../actions/index';
+
+class BookList extends Component{
+
+  renderList(){
+    return this.props.books.map((book) => {
+      return(
+        <li key = {book.title} className = "list-group-item">{book.title}</li>
+      )
+    })
+  }
+
+  render(){
+    return(
+      <ul className="list-group col-sm-4">
+        {this.renderList()}
+      </ul>
+    )
+  }
+}
+
+
+function mapStateToProps(state) {
+  // Whatever is returned will show up as props
+  // inside of BookList
+  return (
+    {books: state.books}
+  )
+}
+
+
+// Promote BookList from a component to a container - it needs
+// to know about this new dispatch method, selectBook. Make it
+// available as a prop.
+
+export default connect(mapStateToProps)(BookList);
